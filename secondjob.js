998ec464196ae0b15d8feedec26a8867c8407d6b
MeteBezgin <metebezgin@gmail.com>
2020-09-10 15:54:32 +0300

    Update secondjob.js

--- secondjob.js
+++ secondjob.js
@@ -2,10 +2,17 @@ const puppeteer = require("puppeteer");
 const baseUrl = "https://catalog.kooymanbv.com/cur/main/";
 const axios = require("axios");
 
+/**
+ *
+ * @param {Object} c - product_id
+ * @returns {Object} - product_data
+ */
+//Function to send a POST request to the site to get a JS object of product data.
 const request = async (c) => {
   return new Promise(async (resolve, reject) => {
     var data = JSON.stringify({ product_id: c });
 
+    //Setting the header of the request
     var config = {
       method: "post",
       url:
@@ -16,12 +23,15 @@ const request = async (c) => {
       },
       data: data,
     };
+    //The response of the request
     let resp = await axios(config);
     resp = JSON.parse(resp.data.d);
+    //The selected variables are not always existing in the product so we dont declare the type of them.
     let product_price = null;
     let product_desc = null;
     let product_features = [];
     if (resp.productinfo) {
+      //Getting the data we want from the response of the request.
       product_price = resp.productinfo.table[0].price;
       product_desc = resp.productinfo.table[0].Description;
     }
@@ -40,6 +50,7 @@ const request = async (c) => {
   });
 };
 
+//A chunking function to chunk arrays to desired size.
 function chunk(arr, chunkSize) {
   var R = [];
   for (var i = 0, len = arr.length; i < len; i += chunkSize)
@@ -47,6 +58,7 @@ function chunk(arr, chunkSize) {
   return R;
 }
 
+//Department numbers of the site to use in url.
 dept_numbers = [
   "16",
   "13",
@@ -72,6 +84,7 @@ dept_numbers = [
   "15",
 ];
 
+//Function to launch browsers and pushing them into a browsers array.
 const browserlauncher = async () => {
   let browsers = [];
   for (a = 1; a < 4; a++) {
@@ -85,128 +98,57 @@ const browserlauncher = async () => {
   return Promise.all(browsers);
 };
 
-const browserlauncher2 = async () => {
-  let browsers = [];
-  for (a = 0; a < 1; a++) {
-    browsers.push(
-      puppeteer.launch({
-        headless: false,
-      })
-    );
-  }
-
-  return Promise.all(browsers);
-};
-
+//A function to open pages on a browser and push them into a pages array, then execute "navigate" function to all of the pages.
 const exec_browser = async (browser, array_index) => {
   let pages = [];
   for (var p = 1; p < 10; p++) {
     pages.push(await browser.newPage());
   }
-  //
-  //  Exexute navigate function on each page, return a promise
-  //
+  //  Execute "navigate" function on each page, return a promise
   let promises = pages.map(function (page, index) {
-    /*await page.setCookie({
-        name: "ASP.NET_SessionId",
-        value: "t5232xauak2nhvag4yebtsyc",
-      });*/
     let array = R[array_index];
     return navigate(page, index, array);
   });
-
-  //
   //  Wait for all the promises to resolve
-  //
-
   return await Promise.all(promises);
 };
 
+//Function to navigate to site and get the product classes for each department.
 const navigate = (page, index, array) => {
   return new Promise(async (resolve, reject) => {
-    return page
-      .goto(
-        `https://catalog.kooymanbv.com/cur/main/product_classes.aspx?dept=${array[index]}`,
-        {
-          waitUntil: "networkidle2",
-          timeout: 0,
-        }
-      )
-      .then(async (e) => {
-        let link = await page.evaluate(() => {
-          let a = document.querySelector("div.mt-10").querySelectorAll("a");
-          console.log(a.length);
-          let b = [];
-          for (i = 0; i < a.length; i++) {
-            b.push(a[i].getAttribute("href"));
+    //Navigation.
+    return (
+      page
+        .goto(
+          `https://catalog.kooymanbv.com/cur/main/product_classes.aspx?dept=${array[index]}`,
+          {
+            waitUntil: "networkidle2",
+            timeout: 0,
           }
-          return b;
-        });
-        return resolve(link);
-      })
-      .catch((err) => {
-        console.log("hata geldi");
-        return resolve(false);
-      });
+        )
+        //Getting the data off the specified selector and pushing it to an array after the promise is resolved.
+        .then(async (e) => {
+          let link = await page.evaluate(() => {
+            let a = document.querySelector("div.mt-10").querySelectorAll("a");
+            console.log(a.length);
+            let b = [];
+            for (i = 0; i < a.length; i++) {
+              b.push(a[i].getAttribute("href"));
+            }
+            return b;
+          });
+          return resolve(link);
+        })
+        //A catch function to catch any errors if any of them occurs.
+        .catch((err) => {
+          console.log("hata geldi");
+          return resolve(false);
+        })
+    );
   });
 };
 
-const exec_browser2 = async (browser, array_index, x) => {
-  let pages = [];
-  for (var p = 0; p < 3; p++) {
-    pages.push(await browser.newPage());
-  }
-  //
-  //  Exexute navigate function on each page, return a promise
-  let Z = x;
-  let promises = pages.map(function (page, index) {
-    return navigate2(page, index, array_index, Z);
-  });
-
-  //
-  //  Wait for all the promises to resolve
-  //
-
-  return await Promise.all(promises);
-};
-
-const navigate2 = (page, index, array_index, Z) => {
-  return new Promise(async (resolve, reject) => {
-    let A = Z[array_index];
-    return page
-      .goto(`${baseUrl}${A[index]}`, {
-        waitUntil: "networkidle2",
-        timeout: 0,
-      })
-      .then(async (e) => {
-        return page.evaluate(async () => {
-          document.querySelector(
-            "#ContentPlaceHolder1_ddItems"
-          ).selectedIndex = 3;
-          await setTimeout(
-            "__doPostBack('ctl00$ContentPlaceHolder1$ddItems','')",
-            0
-          );
-        });
-      })
-      .then(async (e) => {
-        let products = await page.evaluate(() => {
-          a = document.querySelectorAll("a.self-center");
-          b = [];
-          for (i = 0; i < a.length; i++) {
-            b.push(a[i].getAttribute("href"));
-          }
-          return b;
-        });
-        return resolve(products);
-      })
-      .catch((err) => {
-        console.log("hata geldi");
-        return resolve(false);
-      });
-  });
-};
-
+//The main function that calls the other functions to return the "Product Classes" as "output".
 const main = async () => {
   R = await chunk(dept_numbers, 9);
   browsers = await browserlauncher();
@@ -222,62 +164,60 @@ const main = async () => {
   return output;
 };
 
+//Function to get all product urls for each Product Class.
 const product_link_ext = async (class_link, browser) => {
   return new Promise(async (resolve, reject) => {
     let page = await browser.newPage();
-    return page
-      .goto(`${baseUrl}${class_link}`, {
-        waitUntil: "networkidle2",
-        timeout: 20000,
-      })
-      .then(async (e) => {
-        await page.select("#ContentPlaceHolder1_ddItems", "60");
-        await page.waitFor(2500);
-      })
-      .then(async (e) => {
-        let page_length = await page.evaluate(() => {
-          return document
-            .querySelector("#ContentPlaceHolder1_labelpaging")
-            .textContent.split("of")[1]
-            .trim();
-        });
-        console.log(page_length);
-        let all_products = [];
-        for (i = 1; i <= parseInt(page_length); i++) {
-          let products = await page.evaluate(async () => {
-            a = document.querySelectorAll("a.self-center");
-            b = [];
-            for (i = 0; i < a.length; i++) {
-              b.push(a[i].getAttribute("href"));
-            }
-            return b;
-          });
-          all_products.push(...products);
-          await page.click("i.rounded-r-sm");
+    //Navigation.
+    return (
+      page
+        .goto(`${baseUrl}${class_link}`, {
+          waitUntil: "networkidle2",
+          timeout: 20000,
+        })
+        //Selecting the button to show 60 items per page and waiting 2.5 secs for it to refresh.
+        .then(async (e) => {
+          await page.select("#ContentPlaceHolder1_ddItems", "60");
           await page.waitFor(2500);
-        }
-        await page.close();
-        return resolve(all_products);
-      })
-      .catch(async (err) => {
-        console.log(err);
-        await page.close();
-        return resolve(false);
-      });
+        })
+        //Getting the page count.
+        .then(async (e) => {
+          let page_length = await page.evaluate(() => {
+            return document
+              .querySelector("#ContentPlaceHolder1_labelpaging")
+              .textContent.split("of")[1]
+              .trim();
+          });
+          //If the page count is greater than 1, after getting all the product links on page 1, navigate to the other pages and get all the data there as well.
+          let all_products = [];
+          for (i = 1; i <= parseInt(page_length); i++) {
+            let products = await page.evaluate(async () => {
+              a = document.querySelectorAll("a.self-center");
+              b = [];
+              for (i = 0; i < a.length; i++) {
+                b.push(a[i].getAttribute("href"));
+              }
+              return b;
+            });
+            all_products.push(...products);
+            await page.click("i.rounded-r-sm");
+            await page.waitFor(2500);
+          }
+          await page.close();
+          return resolve(all_products);
+        })
+        .catch(async (err) => {
+          console.log(err);
+          await page.close();
+          return resolve(false);
+        })
+    );
   });
 };
 
-const main2 = async (Z) => {
-  let browsers = await browserlauncher2();
-
-  let promises = browsers.map((browser, index) => {
-    return exec_browser2(browser, index, Z);
-  });
-  let output = await Promise.all(promises);
-  return output;
-};
-
-const main3 = async () => {
+//The second main function to call the first one and get classes, then get product links and then send a POST request for each "product_id"
+//and return a JS object containing the product data.
+const main2 = async () => {
   let classes = await main();
   let Y = chunk(classes, 10);
   let browser = await puppeteer.launch({ headless: false });
@@ -301,13 +241,4 @@ const main3 = async () => {
   T = await Promise.all(T);
   console.log(T);
 };
-
-const deneme = async () => {
-  let classes = await main();
-  console.log(classes);
-  let Z = chunk(classes, 10);
-  let sonuc = await main2(Z);
-  console.log(sonuc);
-};
-
-main3();
+main2();
